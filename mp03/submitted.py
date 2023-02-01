f1eb7af1ac65e7cd4710f78aa86a44ac97f1ca46
Everloom-129 <Everloom-129>
2023-02-01 13:15:10 -0600

    start working on mp3

---
+++ mp03/submitted.py
@@ -0,0 +1,56 @@
+'''
+This is the module you'll submit to the autograder.
+
+There are several function definitions, here, that raise RuntimeErrors.  You should replace
+each "raise RuntimeError" line with a line that performs the function specified in the
+function's docstring.
+'''
+import numpy as np
+
+def k_nearest_neighbors(image, train_images, train_labels, k):
+    '''
+    Parameters:
+    image - one image
+    train_images - a list of N images
+    train_labels - a list of N labels corresponding to the N images
+    k - the number of neighbors to return
+
+    Output:
+    neighbors - a list of k images, the k nearest neighbors of image
+    labels - a list of k labels corresponding to the k images
+    '''
+
+    raise RuntimeError('You need to write this part!')
+
+
+def classify_devset(dev_images, train_images, train_labels, k):
+    '''
+    Parameters:
+    dev_images (list) -M images
+    train_images (list) -N images
+    train_labels (list) -N labels corresponding to the N images
+    k (int) - the number of neighbors to use for each dev image
+
+    Output:
+    hypotheses (list) -one majority-vote labels for each of the M dev images
+    scores (list) -number of nearest neighbors that voted for the majority class of each dev image
+    '''
+    
+    raise RuntimeError('You need to write this part!')
+
+
+def confusion_matrix(hypotheses, references):
+    '''
+    Parameters:
+    hypotheses (list) - a list of M labels output by the classifier
+    references (list) - a list of the M correct labels
+
+    Output:
+    confusions (list of lists, or 2d array) - confusions[m][n] is 
+    the number of times reference class m was classified as
+    hypothesis class n.
+    accuracy (float) - the computed accuracy
+    f1(float) - the computed f1 score from the matrix
+    '''
+
+    raise RuntimeError('You need to write this part!')
