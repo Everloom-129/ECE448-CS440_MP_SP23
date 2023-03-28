9c064b70638944c8d847961ee8e26e7e50d619d4
everloom <Everloom-129>
2023-03-28 01:27:54 -0500

    finished baseline, todo viterbi

--- mp08/submitted.py
+++ mp08/submitted.py
@@ -14,8 +14,9 @@ import math
 from collections import defaultdict, Counter
 from math import log
 import numpy as np
-
+# import utils
 # define your epsilon for laplace smoothing here
+epsilon = 2
 
 def baseline(train, test):
     '''
@@ -25,8 +26,32 @@ def baseline(train, test):
     output: list of sentences, each sentence is a list of (word,tag) pairs.
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     '''
-    raise NotImplementedError("You need to write this part!")
+    tagger = {}
+    output = []
+    tag_time = Counter()
+    for sentence in train:
+        for word,tag in sentence:
+            if word not in tagger:
+                tagger[word] = Counter()
+            tagger[word][tag] += 1
+            tag_time[tag] += 1
+    # Determine the most frequent tag for each word in the training data
+    # print(tagger)
+    most_tag = {}
+    for word, tag_counter in tagger.items():
+        most_tag[word] = tag_counter.most_common(1)[0][0]
+    unseen_tag = tag_time.most_common(1)[0][0]
 
+    # new_dev = utils.strip_tags(test)
+    for sentence in test:
+        #TypeError: unhashable type: 'list'
+        new_sentence = [(word, most_tag.get(word, unseen_tag)) for word in sentence]
+        # new_sentence = []
+        # for word in sentence:
+        #     word_pair = (word,most_tag.get(word, unseen_tag))
+        #     new_sentence.append(word_pair)
+        output.append(new_sentence)
+    return output
 
 def viterbi(train, test):
     '''
@@ -36,7 +61,32 @@ def viterbi(train, test):
     output: list of sentences with tags on the words
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     '''
-    raise NotImplementedError("You need to write this part!")
+    tagger = {}
+    output = []
+    tag_time = Counter()
+    for sentence in train:
+        for word,tag in sentence:
+            if word not in tagger:
+                tagger[word] = Counter()
+            tagger[word][tag] += 1
+            tag_time[tag] += 1
+    # Determine the most frequent tag for each word in the training data
+    # print(tagger)
+    most_tag = {}
+    for word, tag_counter in tagger.items():
+        most_tag[word] = tag_counter.most_common(1)[0][0]
+    unseen_tag = tag_time.most_common(1)[0][0]
+
+    # new_dev = utils.strip_tags(test)
+    for sentence in test:
+        #TypeError: unhashable type: 'list'
+        new_sentence = [(word, most_tag.get(word, unseen_tag)) for word in sentence]
+        # new_sentence = []
+        # for word in sentence:
+        #     word_pair = (word,most_tag.get(word, unseen_tag))
+        #     new_sentence.append(word_pair)
+        output.append(new_sentence)
+    return output
 
 
 def viterbi_ec(train, test):
@@ -47,7 +97,32 @@ def viterbi_ec(train, test):
     output: list of sentences, each sentence is a list of (word,tag) pairs.
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     '''
-    raise NotImplementedError("You need to write this part!")
+    tagger = {}
+    output = []
+    tag_time = Counter()
+    for sentence in train:
+        for word,tag in sentence:
+            if word not in tagger:
+                tagger[word] = Counter()
+            tagger[word][tag] += 1
+            tag_time[tag] += 1
+    # Determine the most frequent tag for each word in the training data
+    # print(tagger)
+    most_tag = {}
+    for word, tag_counter in tagger.items():
+        most_tag[word] = tag_counter.most_common(1)[0][0]
+    unseen_tag = tag_time.most_common(1)[0][0]
+
+    # new_dev = utils.strip_tags(test)
+    for sentence in test:
+        #TypeError: unhashable type: 'list'
+        new_sentence = [(word, most_tag.get(word, unseen_tag)) for word in sentence]
+        # new_sentence = []
+        # for word in sentence:
+        #     word_pair = (word,most_tag.get(word, unseen_tag))
+        #     new_sentence.append(word_pair)
+        output.append(new_sentence)
+    return output
 
 
 
