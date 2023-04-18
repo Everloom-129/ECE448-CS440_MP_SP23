88ae1e5104c3ea4351d1a93b5909383cbe6530f2
everloom <Everloom-129>
2023-04-18 00:11:36 -0500

    finished, to debug

--- mp10/submitted.py
+++ mp10/submitted.py
@@ -1,10 +1,4 @@
-'''
-This is the module you'll submit to the autograder.
 
-There are several function definitions, here, that raise RuntimeErrors.  You should replace
-each "raise RuntimeError" line with a line that performs the function specified in the
-function's docstring.
-'''
 import numpy as np
 
 epsilon = 1e-3
@@ -15,10 +9,43 @@ def compute_transition_matrix(model):
     model - the MDP model returned by load_MDP()
 
     Output:
-    P - An M x N x 4 x M x N numpy array. P[r, c, a, r', c'] is the probability that the agent will move from cell (r, c) to (r', c') if it takes action a, where a is 0 (left), 1 (up), 2 (right), or 3 (down).
+    P - An M x N x 4 x M x N numpy array. 
+    P[r, c, a, r', c'] is the probability 
+    that the agent will move from cell (r, c) to (r', c')
+    if it takes action a, where a is 0 (left), 1 (up), 2 (right), or 3 (down).
     '''
-    raise RuntimeError("You need to write this part!")
+    M,N = model.M, model.N
+    P = np.zeros((M,N,4,M,N))
+    for r in range(M):
+        for c in range(N):
+            if not model.T[r, c]:
+                for a in range(4):
+                    movements = []
+                    if a == 0:  # left
+                        movements = [(0, -1), (1, 0), (-1, 0)]
+                    elif a == 1:  # up
+                        movements = [(-1, 0), (0, -1), (0, 1)]
+                    elif a == 2:  # right
+                        movements = [(0, 1), (-1, 0), (1, 0)]
+                    elif a == 3:  # down
+                        movements = [(1, 0), (0, 1), (0, -1)]
+                    # model.D[r, c, 0]: front, 
+                    # model.D[r, c, 1]: left (counter-clockwise)
+                    # model.D[r, c, 2]: right (clockwise)
+                    for idx, (dr, dc) in enumerate(movements):
+                        r_next = r + dr
+                        c_next = c + dc
+                        # Boundary check
+                        Bound = r_next < 0 or r_next >= model.M or c_next < 0 or c_next >= model.N 
+                        if Bound or model.W[r_next, c_next]:
+                            r_next, c_next = r, c
 
+                        P[r, c, a, r_next, c_next] = model.D[r, c, idx]
+            else:
+                P[r, c, :, :, :] = 0
+
+    return P
+         
 def update_utility(model, P, U_current):
     '''
     Parameters:
@@ -29,7 +56,26 @@ def update_utility(model, P, U_current):
     Output:
     U_next - The updated utility function, which is an M x N array
     '''
-    raise RuntimeError("You need to write this part!")
+    M,N = model.M, model.N
+    U_next = np.zeros((M, N))
+
+    for r in range(M):
+        for c in range(N):
+
+            best_value = -np.inf
+            for a in range(4):
+                value = 0
+                for r_next in range(M):
+                    for c_next in range(N):
+                        value += P[r, c, a, r_next, c_next] * U_current[r_next, c_next]
+
+                best_value = max(best_value, value)
+
+            U_next[r, c] = model.R[r, c] + model.gamma * best_value
+
+    return U_next    
+
+
 
 def value_iteration(model):
     '''
@@ -39,7 +85,28 @@ def value_iteration(model):
     Output:
     U - The utility function, which is an M x N array
     '''
-    raise RuntimeError("You need to write this part!")
+    max_iterations = 100
+
+    # Compute the transition matrix
+    P = compute_transition_matrix(model)
+
+    # Initialize the utility function
+    M,N = model.M, model.N
+    U_current = np.zeros((M, N))
+
+    for _ in range(max_iterations):
+        # Update the utility function
+        U_next = update_utility(model, P, U_current)
+
+        # Check for convergence
+        if np.all(np.abs(U_next - U_current) < epsilon):
+            break
+
+        # Update U_current for the next iteration
+        U_current = U_next
+    print(_)
+    return U_current
+    
 
 if __name__ == "__main__":
     import utils
